fc227386bc429c301f4381e5aea44e862b4f7be0
sarveshhh-m <sarveshsavaliya1541@gmail.com>
2025-04-29 22:20:14 +0530

    setup: Icon setup with react-native-svg and react native lucide

--- app/screens/Example.tsx
+++ app/screens/Example.tsx
@@ -1,18 +1,30 @@
-import { View, Text, StyleSheet, Button } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  Platform,
+  StatusBar,
+} from "react-native";
 import React from "react";
 import { useTheme } from "@/utils/ThemeContext";
+import { Thermometer } from "lucide-react-native";
 
 const Example = () => {
-  const { theme, toggleTheme } = useTheme();
+  const { theme, toggleTheme, isDarkMode } = useTheme();
 
   return (
     <View style={[styles.container, { backgroundColor: theme.background }]}>
+      {Platform.OS == "android" && (
+        <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
+      )}
       <Text
         onPress={toggleTheme}
         style={{ color: theme.textSecondary, fontWeight: "bold", fontSize: 24 }}
       >
         Example
       </Text>
+      <Thermometer size={40} color={theme.accent} />
     </View>
   );
 };
