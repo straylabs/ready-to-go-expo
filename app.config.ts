34ab3128e37a79f3707b31f6af44618549ed920b
Sarvesh Savaliya <sarveshsavaliya@Sarveshs-Mac-mini.local>
2025-04-29 10:37:43 +0530

    first commit

---
+++ app.config.ts
@@ -0,0 +1,18 @@
+import { ExpoConfig } from "expo/config";
+
+const config: ExpoConfig = {
+  name: "cro",
+  slug: "cro",
+  version: "1.0.0",
+  icon: "./branding/icon.png",
+  android: {
+    package: "com.straylabs.cro",
+    versionCode: 1,
+  },
+  ios: {
+    bundleIdentifier: "com.straylabs.cro",
+    buildNumber: "1",
+  },
+};
+
+export default config;
--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,14 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
+
+const projectRoot = __dirname;
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+config.resolver.alias = {
+  "@": path.resolve(projectRoot, "app"),
+};
 
 module.exports = config;
