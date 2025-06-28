075961698b45d241ec525af1f183b0daf54469ff
sarveshsavaliya <sarveshsavaliya1541@gmail.com>
2025-06-28 10:05:35 +0530

    feat: upgraded to latest expo version

--- app.config.ts
+++ app.config.ts
@@ -6,13 +6,20 @@ const config: ExpoConfig = {
   version: "1.0.0",
   icon: "./branding/icon.png",
   newArchEnabled: true,
+  userInterfaceStyle:'automatic',
   android: {
     package: "com.straylabs.boilerplate",
     versionCode: 1,
+    edgeToEdgeEnabled: true,
   },
   ios: {
     bundleIdentifier: "com.straylabs.boilerplate",
     buildNumber: "1",
+    icon:{
+      light: "./branding/icon-light.png",
+      dark: "./branding/icon-dark.png",
+
+    }
   },
   web: {
     bundler: "metro",
